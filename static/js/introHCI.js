d912d318c35bb92d951c39b256fb5759ea83c1c8
Tiffany Su <ttsu@ucsd.edu>
2018-01-25 01:47:36 -0800

    added javascript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,25 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Michael Bernstein");
+		$("#testjs").text("Something Different");
+		$(".jumbotron p").addClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+   var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $("#project1").fadeOut();
+    }
 }
